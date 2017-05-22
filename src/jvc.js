b81f30aa1b0dc40efbc4646a363678667d0993fe
Sylvestre <sbouchot@enssat.fr>
2017-05-22 13:08:48 -0400

    Sometimes maxpage function fails, rewrite it, add a queue of message when the bot fails to post

--- src/jvc.js
+++ src/jvc.js
@@ -5,6 +5,7 @@ const request = require('request-promise');
 const crypto = require('crypto');
 const moment = require('moment');
 const cheerio = require('cheerio');
+const debug = require('debug')('node-jeuxvideo-bot-api');
 
 class JvcBot extends EventEmitter {
 
@@ -26,10 +27,11 @@ class JvcBot extends EventEmitter {
     this.options.username = options.username || '';
     this.options.password = options.password || '';
     this.options.topicURLWatcher = options.topicURLWatcher || '';
-    this.options.topicURLWatcher = this.options.topicURLWatcher.split('/').filter(function(elt, i){
-      if(i > 2) {
+    this.options.topicURLWatcher = this.options.topicURLWatcher.split('/').filter(function (elt, i) {
+      if (i > 2) {
         return elt;
       }
+      return '';
     }).join('/');
     this.options.delayBetweenScrap = options.delayBetweenScrap || 10000;
     this.options.baseApiUrl = options.baseApiUrl || 'https://api.jeuxvideo.com/v4/';
@@ -40,12 +42,32 @@ class JvcBot extends EventEmitter {
     this.maxPage = 1;
     this.runningScrape = false;
     this.initScrapeFinished = false;
+    this.messageQueue = [];
     this.startPolling();
     const ctx = this;
     this.intervalPolling = setInterval(() => { ctx.startPolling(); }, this.options.delayBetweenScrap);
     if (!this.options.watchOnly) {
       this.login();
     }
+  }
+
+  /**
+   * Return max page of topic
+   * @param  {Cheerio} $
+   * @param  {Node} linkPagination
+   * @return {Integer}
+   */
+  getPageMax($, linkPagination) {
+    let max = 0;
+    for (let i = 0; i < linkPagination.length; i++) {
+      const currentNode = parseInt($(linkPagination[i]).text(), 10);
+      if (!isNaN(currentNode)) {
+        if (currentNode > max) {
+          max = currentNode;
+        }
+      }
+    }
+    return max;
   }
 
   /**
@@ -60,19 +82,22 @@ class JvcBot extends EventEmitter {
     paths[3] = i;
     const newPath = paths.join('-');
     return this.request(newPath).then((resp) => {
-        if(resp){
-          const $ = cheerio.load(resp, { ignoreWhitespace: true });
-          $('.bloc-message-forum').each(function () {
-            ctx.existingPosts.push($(this).attr('data-id'));
-          });
-          const linkPagination = $('div.bloc-liste-num-page').find('span a');
-          ctx.maxPage = parseInt($(linkPagination[linkPagination.length - 1]).text(), 10) + 1;
-          if (i === ctx.maxPage) {
-            ctx.initScrapeFinished = true;
-            ctx.emit('ready');
-          }
-          return resp;          
+      if (resp) {
+        const $ = cheerio.load(resp, { ignoreWhitespace: true });
+        $('.bloc-message-forum').each(function () {
+          ctx.existingPosts.push($(this).attr('data-id'));
+        });
+        const maxPage = ctx.getPageMax($, $('div.bloc-liste-num-page').find('span a'));
+        const currentPage = parseInt($($('.page-active')[0]).text(), 10);
+        ctx.maxPage = currentPage > maxPage ? currentPage : maxPage;
+        if (i === ctx.maxPage) {
+          ctx.initScrapeFinished = true;
+          ctx.emit('ready');
         }
+        return Promise.resolve();
+      }
+      debug('[WARN] Crawl fails : %s', newPath);
+      return Promise.reject();
     });
   }
 
@@ -88,7 +113,7 @@ class JvcBot extends EventEmitter {
     paths[3] = i;
     const newPath = paths.join('-');
     return this.request(newPath).then((resp) => {
-      if(resp){
+      if (resp) {
         const $ = cheerio.load(resp, { ignoreWhitespace: true });
         $('.bloc-message-forum').each(function () {
           const author = $(this).find('.user-avatar-msg').attr('alt');
@@ -96,18 +121,21 @@ class JvcBot extends EventEmitter {
             const post = {
               id: $(this).attr('data-id'),
               message: $(this).find('.txt-msg.text-enrichi-forum').text(),
-              author: author
+              author
             };
             ctx.emit('message', post);
             ctx.existingPosts.push(post.id);
           }
         });
-        const linkPagination = $('div.bloc-liste-num-page').find('span a');
-        const maxPage = parseInt($(linkPagination[linkPagination.length - 1]).text(), 10) + 1;
-        ctx.maxPage = isNaN(maxPage) ? ctx.maxPage : maxPage;
+        const maxPage = ctx.getPageMax($, $('div.bloc-liste-num-page').find('span a'));
+        const currentPage = parseInt($($('.page-active')[0]).text(), 10);
+        ctx.maxPage = currentPage > maxPage ? currentPage : maxPage;
+        return Promise.resolve();
       }
+      debug('[WARN] Crawl fails : %s', newPath);
+      return Promise.reject();
     }).catch((err) => {
-      console.log(err);
+      Promise.reject(err);
     });
   }
 
@@ -119,25 +147,34 @@ class JvcBot extends EventEmitter {
       const ctx = this;
       this.runningScrape = true;
       this.request(this.options.topicURLWatcher).then((resp) => {
-        if(resp){
-          const $ = cheerio.load(resp, { ignoreWhitespace: true });        
-          const linkPagination = $('div.bloc-liste-num-page').find('span a');
-          const maxPage = parseInt($(linkPagination[linkPagination.length - 2]).text(), 10);
-          ctx.maxPage = isNaN(maxPage) ? 1 : maxPage;
+        if (resp) {
+          const $ = cheerio.load(resp, { ignoreWhitespace: true });
+          const maxPage = ctx.getPageMax($, $('div.bloc-liste-num-page').find('span a'));
+          const currentPage = parseInt($($('.page-active')[0]).text(), 10);
+          ctx.maxPage = currentPage > maxPage ? currentPage : maxPage;
           if (ctx.maxPage !== 1) {
+            debug('[INFO] There are %d pages.', ctx.maxPage);
             return ctx.retrieveBulkPosts(this.options.topicURLWatcher, ctx.maxPage).then();
           }
           $('.bloc-message-forum').each(function () {
             ctx.existingPosts.push($(this).attr('data-id'));
-          });        
+          });
           ctx.initScrapeFinished = true;
           ctx.emit('ready');
-          return resp;
+          return Promise.resolve();
         }
+        debug('[WARN] First crawl fails : %s', ctx.options.topicURLWatcher);
+        this.runningScrape = false;
+        return Promise.reject();
       });
     }
     if (this.initScrapeFinished) {
       this.retrievePost(this.options.topicURLWatcher, this.maxPage).then();
+    }
+    if (this.messageQueue.length > 0) {
+      debug('[INFO] Message queue processed by routine.');
+      const message = this.messageQueue.pop();
+      return this.sendMessage(message.message, message.topicId);
     }
   }
 
@@ -150,7 +187,7 @@ class JvcBot extends EventEmitter {
   request(path, data = false, forumApi = false) {
     const method = data ? 'POST' : 'GET';
     const baseUrl = forumApi ? 'https://api.jeuxvideo.com/' : this.options.baseUrl;
-    const url = `${ baseUrl + path}?${Math.floor(Date.now() / 1000)}`;
+    const url = baseUrl + path;
     const options = {
       method,
       url,
@@ -213,21 +250,24 @@ class JvcBot extends EventEmitter {
    * @return {Promise}
    */
   login() {
-    if(this.options.loginFromCookie.coniunctio){
-        const loginCookie = request.cookie(`coniunctio=${this.options.loginFromCookie.coniunctio}`);
-        this.cookieJar.setCookie(loginCookie, this.options.baseApiUrl.replace('v4/', ''));
-        return Promise.resolve();
+    if (this.options.loginFromCookie.coniunctio) {
+      const loginCookie = request.cookie(`coniunctio=${this.options.loginFromCookie.coniunctio}`);
+      this.cookieJar.setCookie(loginCookie, this.options.baseApiUrl.replace('v4/', ''));
+      debug('[INFO] Login by cookie done.');
+      return Promise.resolve();
     }
     if (this.options.username && this.options.password) {
       return this.requestAPI('accounts/login', { alias: this.options.username, password: this.options.password }).then(() => {
+        debug('[INFO] Login by API done.');
         return Promise.resolve();
-      });        
+      });
     }
+    debug('[ERROR] Login failed');
     return Promise.reject('The bot needs your password and login.');
   }
 
   /**
-   * sendMessage on the watched topic
+   * Send message on the watched topic, if fails put message in a queue
    * @param  {String} message
    * @return {Promise}
    */
@@ -235,24 +275,51 @@ class JvcBot extends EventEmitter {
     if (this.options.watchOnly) return Promise.reject('Watch mode scope activated. Please remove it.');
     if (!message) return Promise.reject('Need a message.');
     const topicId = this.options.topicURLWatcher.split('-')[2];
+    const ctx = this;
     return this.request(`forums/create_message.php?id_topic=${topicId}`, false, true).then((resp) => {
-      const $ = cheerio.load(resp);
-      const inputs = $('.form-post-msg').find('input');
-      const payload = {};
-      inputs.each(function () {
-        payload[$(this).attr('name')] = $(this).attr('value');
-      });
-      payload.message_topic = message;
-      this.request(`forums/create_message.php?id_topic=${topicId}`, payload, true).then((r) => {
-        if (r.indexOf('Une erreur est survenue') > -1 || r.indexOf('Vous devez vous connecter pour répondre au sujet') > -1) {
-          return Promise.reject('[ERROR] Can\'t post in the topic, maybe you are not connected ...');
-        }
-        return Promise.resolve();
-      });
+      if (resp) {
+        const $ = cheerio.load(resp);
+        const inputs = $('.form-post-msg').find('input');
+        const payload = {};
+        inputs.each(function () {
+          payload[$(this).attr('name')] = $(this).attr('value');
+        });
+        payload.message_topic = message;
+        return this.request(`forums/create_message.php?id_topic=${topicId}`, payload, true).then((r) => {
+          if (r) {
+            if (r.toLowerCase().indexOf('erreur') > -1 || r.indexOf('Une erreur est survenue') > -1 ||
+                r.indexOf('Vous devez vous connecter pour répondre au sujet') > -1) {
+              ctx.messageQueue.push({ message, topicId });
+              return Promise.reject('[ERROR] Post in the topic. Reason : maybe you are not connected or delay between two post is too small ...');
+            }
+            debug('[INFO] Message posted');
+            return Promise.resolve();
+          }
+          ctx.messageQueue.push({ message, topicId });
+          debug('[WARN] Seems crawl fails, message put in queue.');
+          return Promise.reject();
+        });
+      } else {
+        ctx.messageQueue.push({ message, topicId });
+        debug('[WARN] Seems crawl fails, message put in queue.');
+        return Promise.reject();
+      }
     });
   }
 
-  getCookieJar(){
+  /**
+   * Retrieve message queue length
+   * @return {Integer} Queue length
+   */
+  getQueueLength() {
+    return this.messageQueue.length;
+  }
+
+  /**
+   * Retrieve cookie jar to save it
+   * @return CookieJar
+   */
+  getCookieJar() {
     return this.cookieJar;
   }
 }
